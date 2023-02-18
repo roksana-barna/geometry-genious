9a9f665d9849cbe021cbdaacd1698be0a47002cb
roksana-barna <roksanabarna1803@gmail.com>
2023-02-18 22:06:59 +0600

    triangle area

--- geometry.js
+++ geometry.js
@@ -50,5 +50,34 @@ document.getElementById('rectcalculate').addEventListener('click',function(){
     triangleAreaCalculate.innerText=letsCalculate;
      const arearesult=document.getElementById('area');
     const result=arearesult+letsCalculate;
+
 });
+// parallaologram
+document.getElementById('parellallo').addEventListener('click',function(){
+    const triangleField=document.getElementById('parainput1');
+    const newTriangleFieldString=triangleField.value;
+    const newTriangleAmount=parseFloat(newTriangleFieldString);
+    triangleField.value=newTriangleAmount;
+    triangleField.value='';
+    
+    const triangleField2=document.getElementById('parainput2');
+    const newTriangleFieldString2nd=triangleField2.value;
+    const newTriangleAmount2nd=parseFloat(newTriangleFieldString2nd);
+    triangleField2.value=newTriangleAmount2nd;
+    triangleField2.value='';
+    
+      const triangleAreaCalculate=document.getElementById('paraArea');
+    
+     const triangleAreaCalculateString=triangleAreaCalculate.value;
+    
+    const triangleAreaCalculates=parseFloat(triangleAreaCalculateString);
+    const addingb=10;
+    const addinh=12;
+    const letsCalculate=addingb*addinh;
+    triangleAreaCalculate.value=letsCalculate;
+     const arearesult=document.getElementById('area');
+    const result=arearesult+letsCalculate;
+
+});
+
 
